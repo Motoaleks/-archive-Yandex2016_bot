84eae0c8fcb3741b5ee7d8f7c31003c714522222
Александр <smile-c@yandex.ru>
2016-03-13 01:30:53 +0300

    refactor
    Aleks Smile

--- StateHandlers/ChooseAccStateHandler.py
+++ StateHandlers/ChooseAccStateHandler.py
@@ -1,34 +1,38 @@
 from StateHandlers.StateHandler import StateHandler
 from DB.DataBase import DataBase
+
+
 class ChooseAccStateHandler(StateHandler):
     def __init__(self):
-        self.id=2
-        self.state_menu=["Назад"]
+        self.id = 2
+        self.state_menu = ["Назад"]
+
     def EnterState(self, ui, stateHandlers):
-        ui.user_state=self.id
+        ui.user_state = self.id
         db = DataBase()
-        cards=db.getAccounts(ui.chat_id)#дичь какая-то
+        cards = db.getAccounts(ui.chat_id)  # дичь какая-то
         if len(cards) == 0:
             ui.sender.sendMessage("У Вас еще нет счетов")
             stateHandlers[StateHandler.State.main].EnterState(ui, stateHandlers)
             return
-        kb=[]
-        ui.cards={}
-        strip=[]
-        for i in range( len(cards)):
+        kb = []
+        ui.cards = {}
+        strip = []
+        for i in range(len(cards)):
             strip.append(cards[i].tostring())
-            ui.cards.update((cards[i].tostring(),cards[i]))
-            if i%2==1:
+            ui.cards.update((cards[i].tostring(), cards[i]))
+            if i % 2 == 1:
                 kb.append[strip]
-                strip=[]
+                strip = []
         kb.append(self.state_menu)
         show_keyboard = {'keyboard': kb}
-        ui.sender.sendMessage("Выберите счёт",reply_markup=show_keyboard)
-        ui.user_state=id
+        ui.sender.sendMessage("Выберите счёт", reply_markup=show_keyboard)
+        ui.user_state = id
+
     def EvaluateState(self, ui, msg, stateHandlers):
-        if msg['text']==self.state_menu[0]:#"Проверить и пополнить баланс"
+        if msg['text'] == self.state_menu[0]:  # "Проверить и пополнить баланс"
             self.action_on_check(msg)
-        elif msg['text']==self.state_menu[1]:#"Добавить счёт"
+        elif msg['text'] == self.state_menu[1]:  # "Добавить счёт"
             pass
         else:
             self.action_on_help()
--- StateHandlers/MainStateHandler.py
+++ StateHandlers/MainStateHandler.py
@@ -1,17 +1,21 @@
 from StateHandlers.StateHandler import StateHandler
+
+
 class MainStateHandler(StateHandler):
     def __init__(self):
-        self.id=StateHandler.State.main
-        self.state_menu=["Проверить и пополнить баланс","Добавить счёт"]
+        self.id = StateHandler.State.main
+        self.state_menu = ["Проверить и пополнить баланс", "Добавить счёт"]
+
     def EnterState(self, ui, stateHandlers):
-        ui.user_state=self.id
-        kb = [[self.state_menu[0]],[self.state_menu[1]]]
+        ui.user_state = self.id
+        kb = [[self.state_menu[0]], [self.state_menu[1]]]
         show_keyboard = {'keyboard': kb}
-        ui.sender.sendMessage("Help text",reply_markup=show_keyboard)
+        ui.sender.sendMessage("Help text", reply_markup=show_keyboard)
+
     def EvaluateState(self, ui, msg, stateHandlers):
-        if msg['text']==self.state_menu[0]:#"Проверить и пополнить баланс"
+        if msg['text'] == self.state_menu[0]:  # "Проверить и пополнить баланс"
             stateHandlers[StateHandler.State.choose_acc].EnterState(ui, stateHandlers)
-        elif msg['text']==self.state_menu[1]:#"Добавить счёт"
+        elif msg['text'] == self.state_menu[1]:  # "Добавить счёт"
             pass
         else:
             self.EnterState(ui)
