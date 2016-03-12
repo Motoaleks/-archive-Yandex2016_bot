4b2ffa83f786c895144b1fd225ebe74c38c73f53
Nikita Tsyrlin <nstsyrlin@gmail.com>
2016-03-12 21:11:35 +0300

    Templates for pay functions

--- yandexAPI/__init__.py
+++ yandexAPI/__init__.py
@@ -1,2 +1,12 @@
-from get_access_token import get_auth_url, set_token, get_token
-from check_balance_using_access_token import do_test_payment
+from yandexAPI.get_access_token import redirect_uri
+#from check_balance_using_access_token import do_test_payment
+
+def get_auth_url(id):
+    return redirect_uri + "?id=" + str(id)
+
+def pay_phone(token, phone, sum):
+    pass
+def pay_troyka(token, num, sum):
+    pass
+def pay_strelka(tokem, num, sum):
+    pass
--- yandexAPI/server.py
+++ yandexAPI/server.py
@@ -6,12 +6,15 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import parse_qs
 import time
 import sys
-from get_access_token import get_auth_url, set_token
-from botwrapper import  bot
+from yandexAPI.get_access_token import get_auth_url, set_token
+from yandexAPI.botwrapper import  bot
+from DB.DataBase import DataBase
 
 HOST_NAME = '' # !!!REMEMBER TO CHANGE THIS!!!
 PORT_NUMBER = 8080 # Maybe set this to 9000.
 LAST_RESORT = "http://google.com/"
+
+db = DataBase()
 
 class RedirectHandler(BaseHTTPRequestHandler):
     def do_HEAD(self):
@@ -25,8 +28,8 @@ class RedirectHandler(BaseHTTPRequestHandler):
         elif(self.path[:9] == '/endpoint'):
             print(self.path)
             ps = parse_qs(self.path[10:])
-            set_token(ps['code'][0], ps['id'][0])
-            bot.sendMessage(ps['id'][0], 'Thanks. Now we can continue with sending money. Send me /give and follow instrucitions.')
+            set_token(ps['code'][0], ps['id'][0], db)
+            bot.sendMessage(ps['id'][0], 'Thanks. Now we can continue with sending money. Send me /help and follow instrucitions.')
             self.send_response(200)
             self.end_headers()
             self.wfile.write('Thank you. Now you can close this window.')
