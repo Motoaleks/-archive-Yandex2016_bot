9a5289860c4a9baceaa42c21c9eb2fdf3658464b
Nikita Tsyrlin <nstsyrlin@gmail.com>
2016-03-12 21:41:01 +0300

    Pay functions

--- yandexAPI/__init__.py
+++ yandexAPI/__init__.py
@@ -1,12 +1,69 @@
 from yandexAPI.get_access_token import redirect_uri
+from yandex_money.api import Wallet
 #from check_balance_using_access_token import do_test_payment
 
 def get_auth_url(id):
     return redirect_uri + "?id=" + str(id)
 
 def pay_phone(token, phone, sum):
-    pass
+    api=Wallet(token)
+    request_options = {
+    "pattern_id": "phone-topup",
+    "phone-number": phone,
+    "amount": sum,
+    "comment": "Payment from bot",
+    "message": "Payment from bot",
+    }
+    request_result = api.request_payment(request_options)
+    print(request_result)
+    process_options={
+        'request_id':request_result['request_id'],
+        'money_source':'wallet'
+    }
+    process_result=api.process_payment(process_options)
+    if process_result['status'] == "success":
+        return True
+    else:
+        return False
+
 def pay_troyka(token, num, sum):
-    pass
+    api=Wallet(token)
+    request_options = {
+    "pattern_id": "10449",
+    "customerNumber": num,
+    "amount": sum,
+    "comment": "Payment from bot",
+    "message": "Payment from bot",
+    }
+    request_result = api.request_payment(request_options)
+    print(request_result)
+    process_options={
+        'request_id':request_result['request_id'],
+        'money_source':'wallet'
+    }
+    process_result=api.process_payment(process_options)
+    if process_result['status'] == "success":
+        return True
+    else:
+        return False
+
 def pay_strelka(tokem, num, sum):
-    pass
+    api=Wallet(token)
+    request_options = {
+    "pattern_id": "10449",
+    "korona_transportCardId": num,
+    "amount": sum,
+    "comment": "Payment from bot",
+    "message": "Payment from bot",
+    }
+    request_result = api.request_payment(request_options)
+    print(request_result)
+    process_options={
+        'request_id':request_result['request_id'],
+        'money_source':'wallet'
+    }
+    process_result=api.process_payment(process_options)
+    if process_result['status'] == "success":
+        return True
+    else:
+        return False
