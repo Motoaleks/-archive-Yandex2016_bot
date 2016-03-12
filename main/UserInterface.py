823037b4facc3282899c008a60a66da815d29be0
AndreySBer <beryukhov-andrey@yandex.ru>
2016-03-12 14:40:00 +0300

    New interface with choosing category
    I want to see CultEvent Class Bleat!!!

--- main/UserInterface.py
+++ main/UserInterface.py
@@ -1,13 +1,27 @@
 # coding=UTF-8
 import telepot
 
-
+'''
+Заглушка для получения состояния пользователя
+'''
 def get_user_state(chat_id):
     return "default"
+'''
+Заглушка для "показать рекомендации"
+'''
+def get_recomendations():
+
+    return []
+
+'''
+Заглушка для получения списка категорий
+'''
+def get_categories():
+    return ['Sample category','Sample category2']
 
 
 class UserInterface(telepot.helper.UserHandler):
-    first_list=('Показать рекомендации','Хочу дёшево','Рядом со мной','По дате','По категориям')
+    main_menu_list=('Показать рекомендации', 'Хочу дёшево', 'Рядом со мной', 'Скоро', 'По категориям')
     def __init__(self, seed_tuple, timeout):
         super(UserInterface, self).__init__(seed_tuple, timeout)
         self.user_state='undefined'
@@ -27,18 +41,26 @@ class UserInterface(telepot.helper.UserHandler):
                 self.action_on_start(msg)
             if msg['text'] == '/help' or msg['text'] == '/help@ticket_buy_bot':
                 self.action_on_help(msg)
-        elif (self.user_state=='first' and msg['text'] in UserInterface.first_list):
-            hide_keyboard = {'hide_keyboard': True}
-            message=msg['text']
-            self.sender.sendMessage(message, reply_markup=hide_keyboard)
-
+        elif (self.user_state=='first' and msg['text'] in UserInterface.main_menu_list):
+            if msg['text']==UserInterface.main_menu_list[4]: #'По категориям'
+                kb =[]
+                for cat in get_categories():
+                    kb.append([cat])
+                show_keyboard = {'keyboard': kb}
+                self.sender.sendMessage("Теперь выбери категорию.",reply_markup=show_keyboard)
+                self.user_state='choose_category'
+            else:
+                hide_keyboard = {'hide_keyboard': True}
+                message=msg['text']
+                self.sender.sendMessage(message, reply_markup=hide_keyboard)
+        
 
     def action_on_start(self, msg):
         self.sender.sendMessage(
             "Привет! Я ticket-buy bot! Я помогу быстро и удобно купить билет на интересующее тебя мероприятие. Жми /help чтобы узнать подробности.")
 
     def action_on_help(self, msg):
-        kb = [[UserInterface.first_list[0]],[UserInterface.first_list[1],UserInterface.first_list[2]],[UserInterface.first_list[3],UserInterface.first_list[4]]]
+        kb = [[UserInterface.main_menu_list[0]],[UserInterface.main_menu_list[1],UserInterface.main_menu_list[2]],[UserInterface.main_menu_list[3],UserInterface.main_menu_list[4]]]
         show_keyboard = {'keyboard': kb}
         self.sender.sendMessage(
             "Билетер (ticket_buy_bot) - это бот для Telegram, который выдаёт информацию о лучших мероприятиях в ближайшее время и помогает купить билет. Чтобы ты не пропустил долгожданное событие, наш бот обязательно напомнит о нем. \n\n"
--- main/__init__.py
+++ main/__init__.py
@@ -2,12 +2,12 @@
 import sys
 import telepot
 from telepot.delegate import per_from_id, create_open
-from main.UserInterface import UserInterface
+from UserInterface import UserInterface
 
 TOKEN = sys.argv[1]
 bot = telepot.DelegatorBot(TOKEN, [(per_from_id(),
                                     create_open(UserInterface,
-                                                timeout=10)), ])
+                                                timeout=3600)), ])
 bot.sendMessage(54016973, 'Bileter bot started')
 print('Listening ...')
 bot.notifyOnMessage(run_forever=True)
