5333f3ddc64eff57e3c9f0c48de13a4e2e26522f
Александр <smile-c@yandex.ru>
2016-03-13 03:16:42 +0300

    BalanceShow 1.0
    Aleks Smile

--- StateHandlers/BalanceShowStateHandler.py
+++ StateHandlers/BalanceShowStateHandler.py
@@ -1,13 +1,23 @@
+from DB.DataBase import DataBase
 from StateHandlers.StateHandler import StateHandler
 from yandexAPI import get_auth_url
+
+
 class BalanceStateHandler(StateHandler):
     def __init__(self):
-        id=0
-        state_menu=[]
+        self.state_menu = [["Пополнить"], ["Назад"]]
+
     def EnterState(self, ui, stateHandlers, account):
-        pass
+        self.account = account
+        ui.user_state = self.id
+        ui.sender.sendMessage("Текущий баланс: " + account.getBalance())
+        ui.sender.sendMessage(reply_marking=self.state_menu)
+
     def EvaluateState(self, ui, msg, stateHandlers):
-        import telepot
-        ui.content_type, ui.chat_type, ui.chat_id = telepot.glance(msg)
-        ui.sender.sendMessage("Привет! Я ticket-buy bot! Я помогу быстро и удобно проверить и пополнить баланс карт Тройка и Стрелка или счёт телефона. Перейди по ссылке и разреши нам доступ)\n"+get_auth_url(ui.chat_id))
-        #TODO:check token
+        if msg['text'] == self.state_menu[0]:
+            stateHandlers[StateHandler.State.inpute_sum].EnterState(ui, stateHandlers, self.account)
+        elif msg['text'] == self.state_menu[1]:
+            stateHandlers[StateHandler.State.choose_acc].EnterState(ui, stateHandlers)
+        else:
+            ui.sender.sendMessage("Неверная команда")
+            stateHandlers[StateHandler.State.balance_show].EnterState(ui, stateHandlers, self.acccount)
